e4d3d9f74f46e0336ea31cf0f0a88c26a1fd91a4
JulianaAstra <julianaas@yandex.ru>
2023-02-01 00:14:56 +0300

    Решила первую задачу

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,21 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = (b ** 2) - (4 * a * c);
+  let root1;
+  let root2;
+
+  if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(root1, root2);
+  } else if (discriminant === 0) {
+    root1 = -b / (2 * a);
+    arr.push(root1);
+  } else {
+    arr = [];
+  }
   return arr;
 }
 
