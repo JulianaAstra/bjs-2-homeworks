2f2f139decaeb4d84bbbbfcb47ea1897a293b877
JulianaAstra <julianaas@yandex.ru>
2023-02-08 13:31:05 +0300

    Исправила недочёт

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,7 @@
 function getArrayParams(...arr) {
   let max = Math.max(...arr);
   let min = Math.min(...arr);
-  let sum = arr.reduce((a, b) => a + b);;
+  let sum = arr.reduce((a, b) => a + b);
   let avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
