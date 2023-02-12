6fca5e3e26e9ce11bef512e8dd0b2cca8d7842be
JulianaAstra <julianaas@yandex.ru>
2023-02-12 21:13:50 +0300

    Решила первую задачу

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length < arr2.length) {
+        let newArr2 = arr1.slice();
+        arr1 = arr2;
+        arr2 = newArr2;
+      }
+    
+      return arr1.every((elem, index) => elem === arr2[index]);
 }
+
 
 function getUsersNamesInAgeRange(users, gender) {
   
