21d794f6bcb999833290324d39297cf1d3d98d44
Julia Karnausova <julianaas@yandex.ru>
2023-02-16 18:22:43 +0300

    refactor first task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,11 +1,5 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length < arr2.length) {
-        let newArr2 = arr1.slice();
-        arr1 = arr2;
-        arr2 = newArr2;
-      }
-    
-      return arr1.every((elem, index) => elem === arr2[index]);
+      return arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
