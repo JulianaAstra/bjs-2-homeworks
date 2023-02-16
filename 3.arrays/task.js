2bd805c819a388dfc9cba6467f80af1d0f0ce7b2
Julia Karnausova <julianaas@yandex.ru>
2023-02-16 13:08:41 +0300

    Решение второй задачи

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,7 +8,8 @@ function compareArrays(arr1, arr2) {
       return arr1.every((elem, index) => elem === arr2[index]);
 }
 
-
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  let filtered = users.filter(man => man.gender === gender);
+  return filtered.reduce((acc, item) => acc + item.age/filtered.length, 0); 
+};
+
