aad462f6f6dd06774c934508deba2e389869a1fe
Julia Karnausova <julianaas@yandex.ru>
2023-02-18 21:29:17 +0300

    Решение первой задачи

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks')) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.hasOwnProperty('marks') || this.marks === []) {
+    return 0;
+  }
+    
+  return this.marks.reduce((acc, item, idx, array) => acc + item / array.length, 0); 
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
