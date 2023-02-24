6f486f5e3e5432100806010beb953a2fbb150ecb
JulianaAstra <julianaas@yandex.ru>
2023-02-24 23:51:54 +0300

    решение первой задачи

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,17 @@
-﻿
+﻿// ## Задача 1. Форматтер чисел
+
+function parseCount (number) {
+    if (Number.parseFloat(number)) {
+        return Number.parseFloat(number);
+    } 
+
+    throw new Error ('Невалидное значение');
+}
+
+function validateCount (count) {
+    try {
+        return parseCount(count);
+    } catch (error) {
+        return error;
+    }
+}
