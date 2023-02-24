bacbfdc4de96302199d1277e499fb3fa66bc7b9b
JulianaAstra <julianaas@yandex.ru>
2023-02-25 01:15:34 +0300

    решение второй задачи

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,4 +1,4 @@
-﻿// ## Задача 1. Форматтер чисел
+﻿// Задача 1. Форматтер чисел
 
 function parseCount (number) {
     if (Number.parseFloat(number)) {
@@ -13,5 +13,44 @@ function validateCount (count) {
         return parseCount(count);
     } catch (error) {
         return error;
+    }
+}
+
+// Задача 2. Треугольник 
+class Triangle {
+    constructor (sideA, sideB, sideC) {
+        if (sideA + sideB < sideC || sideB + sideC < sideA 
+            || sideA + sideC < sideB) {
+            throw new Error('Треугольник с такими сторонами не существует');        
+        }
+        this['Side A'] = sideA;
+        this['Side B'] = sideB;
+        this['Side C'] = sideC;
+    }
+
+    get perimeter() {
+        return Number((this['Side A'] + this['Side B'] + this['Side C']).toFixed(3));
+    }
+
+    get area() {
+        const halfPerimeter = this.perimeter / 2;
+        const result = Math.sqrt(halfPerimeter * (halfPerimeter - this['Side A']) * (halfPerimeter - this['Side B']) * (halfPerimeter - this['Side C']));
+        return Number(result.toFixed(3));
+    }
+}
+
+function getTriangle (sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
     }
 }
