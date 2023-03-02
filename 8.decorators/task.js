ad47856f3cba4936defb7f6d338d270dd3a32c97
JulianaAstra <julianaas@yandex.ru>
2023-03-02 17:10:49 +0300

    добавит решение 1 задачи

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,28 @@
 //Задача № 1
+const sum = (a, b) => a + b;
+
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper (...args) {
+    const hash = md5(args);
+    let hashIndex = cache.findIndex((item) => item['hash'] === hash);
+
+    if (hashIndex >= 0) {
+        return `Из кэша: ${cache[hashIndex]['value']}`;
+    }
+
+    const result = func(...args);
+
+    cache.push({'hash': hash, 'value': result});
+        if (cache.length > 5) { 
+          cache.shift();
+       }
+    return `Вычисляем: ${result}`;
+  }
+  return wrapper;
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
